b67dbe588cad2248175a57911d6191065138bce7
Lucas Lim <138953777+lucasucal@users.noreply.github.com>
2026-09-18 14:53:51 +0100

    Bump SDK version to 0.1.74

--- overmind/overmind/__init__.py
+++ overmind/overmind/__init__.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-__version__ = "0.1.73"
+__version__ = "0.1.74"
 
 from .client import Client, ModelDeleted, OvermindInferenceError
 
