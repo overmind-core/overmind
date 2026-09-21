529f8f2a72513ddac174109e6a217af74821b795
Pritam Soni <23050213+pritamsoni-hsr@users.noreply.github.com>
2026-09-21 10:14:55 +0530

    add db call in healthcheck (#117)

--- overbae/api/health.py
+++ overbae/api/health.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -8,4 +9,6 @@ from rest_framework.response import Response
 @permission_classes([AllowAny])
 @extend_schema(exclude=True)
 def health_check(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT 1")
     return Response({"status": "healthy"})
